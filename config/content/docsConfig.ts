2ebcaa5bf4c5e9eab9ff447a253d5d2246a5d8e0
Andrew Serbin <serbinadrian@outlook.com>
2024-08-02 11:09:20 +0300

    [SPT-480] Added docs config template

---
+++ config/content/docsConfig.ts
@@ -0,0 +1,31 @@
+export default [
+    {
+        text: "AI Platform Ecosystem",
+        textIconID: "marketplace-icon",
+        imageSrc: "",
+        description:
+            "Explore, publish, and integrate AI services on AI Marketplace's docs.",
+        items: [
+            {
+                text: "Getting started",
+                textIconID: "lightning-icon",
+                link: "",
+                primary: true,
+                // no children
+            },
+            {
+                // no icon
+                // no section link
+                // not primary
+                text: "Daemon",
+                items: [
+                    {
+                        // no icon
+                        text: "Setup daemon",
+                        link: "...",
+                    },
+                ],
+            },
+        ],
+    },
+];
